8777955df5e5d1004c6b15579a7e309d716aa767
Usuario <Usuario@DESKTOP-U4P8J7O>
2023-08-28 20:36:22 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar las cantidad de minutos: ");
+            int minutos = int.Parse(Console.ReadLine());
+
+            int horas = minutos / 60;
+            int minutosRestantes = minutos % 60 ;
+
+            Console.WriteLine($"{minutos} minutos son {horas} horas y {minutosRestantes} minutos.");
+            Console.ReadKey();
+        }
+    }
+}
